8d03b77b7ee43aad7adc28511bb9c3846a070f9d
aNOOBis666 <mr.zooko@mail.ru>
2021-12-18 16:21:55 +0300

    Retrofit added enterFragment syncing updated

--- app/src/main/java/com/length/icthack3/ui/main/screens/EnterFragment.kt
+++ app/src/main/java/com/length/icthack3/ui/main/screens/EnterFragment.kt
@@ -2,11 +2,14 @@ package com.length.icthack3.ui.main.screens
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.length.icthack3.R
 import com.length.icthack3.ui.main.viewModels.EnterViewModel
 import com.length.icthack3.ui.main.viewModels.RegistrationViewModel
@@ -24,6 +27,19 @@ class EnterFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val userToken = view.findViewById<TextInputEditText>(R.id.editLogin)
+        val navigateToMainButton = view.findViewById<MaterialButton>(R.id.toMainButton)
+        val navigateToRegistrationButton = view.findViewById<TextureView>(R.id.toRegistrationFragment)
+
+        navigateToRegistrationButton.setOnClickListener {
+            navigateToRegistrationFragment()
+        }
+
+        navigateToMainButton.setOnClickListener {
+            navigateToMainFragment()
+            TODO("Make request on firebase to check authorize")
+        }
+
     }
 
     private fun navigateToRegistrationFragment(){
