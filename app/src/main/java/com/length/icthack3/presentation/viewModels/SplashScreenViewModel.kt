c23deb153a61724785d080103912dcd5250d41ca
aNOOBis666 <mr.zooko@mail.ru>
2021-12-18 20:09:17 +0300

    Changes

--- app/src/main/java/com/length/icthack3/presentation/viewModels/SplashScreenViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.length.icthack3.presentation.viewModels
-
-import androidx.lifecycle.ViewModel
-
-class SplashScreenViewModel: ViewModel() {
-    fun splashScreenController(){
-//        TODO("Make request to check authorize")
-    }
-}
