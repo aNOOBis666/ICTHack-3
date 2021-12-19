ff9b0a0ad74d05663d40b2b8e4e29b4cce049511
Egor <gyvacha@gmail.com>
2021-12-19 11:08:15 +0300

    Shop fragment

--- app/src/main/java/com/length/icthack3/domain/model/Animal.kt
+++ app/src/main/java/com/length/icthack3/domain/model/Animal.kt
@@ -17,12 +17,18 @@ data class Animal(
         TYPE_GOOSE -> 2.0
         TYPE_GOAT -> 3.0
         else -> 0.0
+    },
+    val cost: Int = when (type) {
+        TYPE_PIG -> 100
+        TYPE_GOOSE -> 200
+        TYPE_GOAT -> 300
+        else -> 0
     }
 ) : Parcelable {
-   companion object {
-       const val TABLE_NAME = "Animal"
-       const val TYPE_PIG = "Pig"
-       const val TYPE_GOOSE = "Goose"
-       const val TYPE_GOAT = "Goat"
-   }
+    companion object {
+        const val TABLE_NAME = "Animal"
+        const val TYPE_PIG = "Pig"
+        const val TYPE_GOOSE = "Goose"
+        const val TYPE_GOAT = "Goat"
+    }
 }
--- app/src/main/java/com/length/icthack3/presentation/screens/ShopFragment.kt
+++ app/src/main/java/com/length/icthack3/presentation/screens/ShopFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,12 +14,17 @@ import androidx.navigation.Navigation
 import com.length.icthack3.R
 import com.length.icthack3.presentation.viewModels.ShopViewModel
 
-class ShopFragment: Fragment() {
-    companion object {
-        fun newInstance() = ShopFragment()
-    }
+class ShopFragment : Fragment() {
 
     private val viewModel: ShopViewModel by viewModels()
+
+    private lateinit var balanceTextView: TextView
+    private lateinit var pigImageView: ImageView
+    private lateinit var gooseImageView: ImageView
+    private lateinit var goatImageView: ImageView
+    private lateinit var buyPigButton: Button
+    private lateinit var buyGooseButton: Button
+    private lateinit var buyGoatButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,14 +35,29 @@ class ShopFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews(view)
+        initBackPress()
+    }
+
+    private fun initViews(view: View) {
+        balanceTextView = view.findViewById(R.id.balance_text_view)
+        pigImageView = view.findViewById(R.id.pig_image_view)
+        gooseImageView = view.findViewById(R.id.goose_image_view)
+        goatImageView = view.findViewById(R.id.goat_image_view)
+        buyPigButton = view.findViewById(R.id.buy_pig_button)
+        buyGooseButton = view.findViewById(R.id.buy_goose_button)
+        buyGoatButton = view.findViewById(R.id.buy_goat_button)
+    }
+
+    private fun initBackPress() {
         val callback: OnBackPressedCallback =
-            object : OnBackPressedCallback(true /* enabled by default */) {
+            object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    val navController = Navigation.findNavController(requireActivity(), R.id.container)
+                    val navController =
+                        Navigation.findNavController(requireActivity(), R.id.container)
                     navController.navigate(R.id.action_shopFragment_to_gameFragment2)
                 }
             }
         requireActivity().onBackPressedDispatcher.addCallback(this@ShopFragment, callback)
-
     }
 }
